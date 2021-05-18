7ef20c4a2311d25514a5a55c1c2ac43bab6e20cf
Tim Etler <tmetler@gmail.com>
2021-05-18 14:45:14 -0700

    Added example of generic interface failure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import { userValidator } from "./validators"
-import { user } from "./schema"
+import { buildDecoder } from "io-ts-transformer"
 
-const age = user.age
+interface FilterQuery<F> {
+  filters?: F
+}
+
+interface Filters {
+  age: number
+}
+
+interface UserQuery extends FilterQuery<Filters> {
+  name: string
+}
+
+export const idValidator = buildDecoder<UserQuery>()
